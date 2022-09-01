ede992b8010fab29ec94ac9b2607906e801e9e2d
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-09-01 08:01:07 +0300

    add js file

--- images/index.js
+++ images/index.js
@@ -1,8 +1,29 @@
-function fetchimage(){
-    fetch("http://localhost:3000/Doctors")
-    .then(res=>res.json())
-    .then((data)=>{
+class formData{
+    constructor(fullname,category,registrationNo,phoneNO,location){
+        this.fullname=fullname;
+        this.category=category;
+        this.registrationNo=registrationNo;
+        this.phoneNO=phoneNO;
+        this.location=location;
+    }
+}
+
+const collectedData=new formData(this)
+ function dataCollection(){
+    document.getElementById("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    fetch("http://localhost:3000/Doctors",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(collectedData)
+    }).then(response=>{
+        return response.json();
+    }).then((data)=>{
         console.log(data)
     })
-}
-fetchimage()
+})}
+document.addEventListener("DOMContentLoaded",()=>{
+dataCollection();
+})
